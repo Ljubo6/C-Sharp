67b3b11457882bf1ba413b6f00acf6fb1d2a0aa8
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-03-30 21:42:34 +0200

    Add exam

---
+++ Technology Fundamentals Final Exam - 16 December 2018/01. Concert/01. Concert.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> bandsNameAndMembers = new Dictionary<string, List<string>>();
+            Dictionary<string, int> bandsNameAndTime = new Dictionary<string, int>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "start of concert")
+            {
+                string[] tokens = input.Split(new[] { "; " }, StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                string bandName = tokens[1];
+
+                if (command == "Add")
+                {
+                    List<string> bandMembers = tokens[2].Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    bandMembers = bandMembers.Distinct().ToList();
+                    if (!bandsNameAndMembers.ContainsKey(bandName))
+                    {
+                        bandsNameAndMembers.Add(bandName, new List<string>());
+                        bandsNameAndMembers[bandName].AddRange(bandMembers);
+                    }
+                    else
+                    {
+                        foreach (var member in bandMembers)
+                        {
+                            if (!bandsNameAndMembers[bandName].Contains(member))
+                            {
+                                bandsNameAndMembers[bandName].Add(member);
+                            }
+                        }
+
+                    }
+                }
+                else if (command == "Play")
+                {
+                    int time = int.Parse(tokens[2]);
+                    if (!bandsNameAndTime.ContainsKey(bandName))
+                    {
+                        bandsNameAndTime.Add(bandName, 0);
+
+                    }
+                    bandsNameAndTime[bandName] += time;
+                }
+
+
+            }
+            string band = Console.ReadLine();
+            Console.WriteLine("Total time: {0}", bandsNameAndTime.Values.Sum());
+            foreach (var kvp in bandsNameAndTime.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine("{0} -> {1}", kvp.Key, kvp.Value);
+            }
+            foreach (var kvp in bandsNameAndMembers)
+            {
+                if (kvp.Key == band)
+                {
+                    Console.WriteLine(kvp.Key);
+                    List<string> bandPeople = kvp.Value;
+                    foreach (var member in bandPeople)
+                    {
+                        Console.WriteLine("=> {0}", member);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals Final Exam - 16 December 2018/02. Song Encryption/02. Song Encryption.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] tokens = input.Split(':');
+                string artist = tokens[0];
+                string song = tokens[1];
+
+                string patternArtist = @"^[A-Z][', a-z]+$";
+                string patternSong = @"^[A-Z, ]+$";
+                Regex regexArtist = new Regex(patternArtist);
+                Regex regexSong = new Regex(patternSong);
+                if (regexArtist.IsMatch(artist) && regexSong.IsMatch(song))
+                {
+                    
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        int key = artist.Length;
+                        if (input[i] != ' ' && input[i] != '\'' && input[i] != ':')
+                        {
+
+                            char symbol = (char)(input[i] + key);
+                            if (char.IsLower(input[i]) && symbol > 'z')
+                            {
+                                key -= 'z' - input[i] + 1;
+                                symbol = (char)('a' + key);
+                                sb.Append(symbol);
+                            }
+                            else if (char.IsUpper(input[i]) && symbol > 'Z')
+                            {
+                                key -= 'Z' - input[i] + 1;
+                                symbol = (char)('A' + key);
+                                sb.Append(symbol);
+                            }
+                           
+                            else
+                            {
+                                sb.Append(symbol);
+                            }
+                        }
+                        else
+                        {
+                            if (input[i] == ':')
+                            {
+                                input = input.Replace(input[i], '@');
+                            }
+                            
+                            sb.Append(input[i]);
+                        }
+
+                    }
+
+                    Console.WriteLine($"Successful encryption: {sb}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+            }
+        }
+    }
+}
