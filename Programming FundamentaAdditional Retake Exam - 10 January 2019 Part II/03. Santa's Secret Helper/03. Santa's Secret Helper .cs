373f1c9d5fd82002e23a12bae41fc07fdaf723b2
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-02 23:49:41 +0300

    Add exam

---
+++ Programming FundamentaAdditional Retake Exam - 10 January 2019 Part II/03. Santa's Secret Helper/03. Santa's Secret Helper .cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03._Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            List<string> validStr = new List<string>();
+            List<string> childGood = new List<string>();
+            while ((input = Console.ReadLine()) != "end")
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char ch = (char)(input[i] - key);
+                    sb.Append(ch);
+
+                }
+                string strToList = sb.ToString();
+                string pattern = @"([^@:!\->]*)@[A-Za-z]+([^@:!\->]*)!([GN])!([^@:!\->]*)";
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(strToList))
+                {
+                    validStr.Add(strToList);
+                }
+                
+            }          
+            string patternName = @"(?<=\@)([A-Za-z]+)";
+            string patternBehavior = @"(?<=\!)[GN](?=\!)";
+            Regex nameRegex = new Regex(patternName);
+            Regex behavierRegex = new Regex(patternBehavior);
+
+            for (int i = 0; i < validStr.Count; i++)
+            {
+                string name = GetMatch(nameRegex,validStr[i]);
+                string behavier = GetMatch(behavierRegex,validStr[i]);
+                if (behavier == "G")
+                {
+                    childGood.Add(name);
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, childGood));
+        }
+
+        private static string GetMatch(Regex reg, string str)
+        {
+            string validString = reg.Match(str).ToString();
+            return validString;
+        }
+    }
+}
---
+++ Programming FundamentaAdditional Retake Exam - 10 January 2019 Part II/04. Santa's New List/04. Santa's New List .cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Santa_s_New_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            string childName = string.Empty;
+            string typeOfToy = string.Empty;
+            int amount = 0;
+            Dictionary<string, int> childNameAndAmount = new Dictionary<string, int>();
+            Dictionary<string, int> toysAndAmount = new Dictionary<string, int>();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split("->");
+                if (tokens[0] != "Remove")
+                {
+                    childName = tokens[0];
+                    typeOfToy = tokens[1];
+                    amount = int.Parse(tokens[2]);
+
+                    if (!childNameAndAmount.ContainsKey(childName))
+                    {
+                        childNameAndAmount.Add(childName,0);
+                    }
+                    childNameAndAmount[childName] += amount;
+
+                    if (!toysAndAmount.ContainsKey(typeOfToy))
+                    {
+                        toysAndAmount.Add(typeOfToy,0);
+                    }
+                    toysAndAmount[typeOfToy] += amount;
+                }
+                else
+                {
+                    childName = tokens[1];
+                    childNameAndAmount.Remove(childName);
+                }
+                
+            }
+            Console.WriteLine("Children:");
+            foreach (var kvp in childNameAndAmount.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+            Console.WriteLine("Presents:");
+            foreach (var kvp in toysAndAmount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
