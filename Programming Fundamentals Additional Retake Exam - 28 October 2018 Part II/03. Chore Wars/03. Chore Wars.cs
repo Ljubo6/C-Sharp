67314281bfbb6b75620d892540698865714190ca
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-03 21:30:05 +0300

    Add exam

---
+++ Programming Fundamentals Additional Retake Exam - 28 October 2018 Part II/03. Chore Wars/03. Chore Wars.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03._Chore_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            int totalSum = 0;
+            Dictionary<string, int> chore = new Dictionary<string, int>();
+            string patternDishes = @"(?<=\<)[a-z0-9]+(?=\>)";
+            string patternCleaning = @"(?<=\[)[A-Z0-9]+(?=\])";
+            string patternLaundry = @"(?<=\{)[\w\W\s]+(?=\})";
+
+            Regex dishesRegex = new Regex(patternDishes);
+            Regex cleaningRegex = new Regex(patternCleaning);
+            Regex laundryRegex = new Regex(patternLaundry);
+            while ((input = Console.ReadLine()) != "wife is happy")
+            {
+                string str = string.Empty;
+                string currentStr = string.Empty;
+                if (dishesRegex.IsMatch(input))
+                {
+                    str = "Doing the dishes";
+                    currentStr = dishesRegex.Match(input).ToString(); ;
+                }
+                else if (cleaningRegex.IsMatch(input))
+                {
+                    str = "Cleaning the house";
+                    currentStr = cleaningRegex.Match(input).ToString();
+                }
+                else if (laundryRegex.IsMatch(input))
+                {
+                    str = "Doing the laundry";
+                    currentStr = laundryRegex.Match(input).ToString();
+                }
+                else
+                {
+                    continue;
+                }
+                int sum = 0;
+                int num = 0;
+
+                for (int i = 0; i < currentStr.Length; i++)
+                {
+                    if (char.IsDigit(currentStr[i]))
+                    {
+                        num = int.Parse(currentStr[i].ToString());
+                        sum += num;
+                    }
+                }
+                totalSum += sum;
+                if (!chore.ContainsKey(str))
+                {
+                    chore.Add(str, 0);
+                }
+                chore[str] += sum;
+            }
+
+            foreach (var kvp in chore.OrderBy(x => x.Key.Length).Take(1))
+            {
+                Console.WriteLine("{0} - {1} min.", kvp.Key, kvp.Value);
+            }
+            foreach (var kvp in chore.OrderBy(x => x.Key.Length).Skip(2).Take(1))
+            {
+                Console.WriteLine("{0} - {1} min.", kvp.Key, kvp.Value);
+            }
+            foreach (var kvp in chore.OrderBy(x => x.Key.Length).Skip(1).Take(1))
+            {
+                Console.WriteLine("{0} - {1} min.", kvp.Key, kvp.Value);
+            }
+
+            Console.WriteLine("Total - {0} min.", totalSum);
+        }
+    }
+}
---
+++ Programming Fundamentals Additional Retake Exam - 28 October 2018 Part II/04. MeTube Statistics/04. MeTube Statistics.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04._MeTube_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<int>> video = new Dictionary<string, List<int>>();
+
+            while ((input = Console.ReadLine()) != "stats time")
+            {
+                if (input.Contains('-'))
+                {
+                    string[] tokens = input.Split('-');
+                    string videoName = tokens[0];
+                    int view = int.Parse(tokens[1]);
+                    if (!video.ContainsKey(videoName))
+                    {
+                        video.Add(videoName, new List<int>());
+                        video[videoName].Add(view);
+                        video[videoName].Add(0);
+                    }
+                    else
+                    {
+                        video[videoName][0] += view;
+                    }
+                }
+                else if (input.Contains(':'))
+                {
+                    string[] tokens = input.Split(':');
+                    string command = tokens[0];
+                    string videoName = tokens[1];
+                    if (command == "like")
+                    {
+                        if (video.ContainsKey(videoName))
+                        {
+                            video[videoName][1]++;
+                        }
+
+                    }
+                    else if (command == "dislike")
+                    {
+                        if (video.ContainsKey(videoName))
+                        {
+                            video[videoName][1]--;
+                        }
+                    }
+                }
+            }
+            string criterion = Console.ReadLine();
+            if (criterion == "by views")
+            {
+                foreach (var kvp in video.OrderByDescending(x => x.Value[0]))
+                {
+                    Console.WriteLine("{0} - {1} views - {2} likes", kvp.Key, kvp.Value[0], kvp.Value[1]);
+                }
+            }
+            else if (criterion == "by likes")
+            {
+                foreach (var kvp in video.OrderByDescending(x => x.Value[1]))
+                {
+                    Console.WriteLine("{0} - {1} views - {2} likes", kvp.Key, kvp.Value[0], kvp.Value[1]);
+                }
+            }
+
+        }
+    }
+}
