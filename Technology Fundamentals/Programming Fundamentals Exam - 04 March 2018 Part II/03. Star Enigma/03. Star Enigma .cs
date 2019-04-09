325baeb7c017b03a27916b391aaf5d54ae9d4b58
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-09 23:12:38 +0300

    Add Exam

---
+++ Technology Fundamentals/Programming Fundamentals Exam - 04 March 2018 Part II/03. Star Enigma/03. Star Enigma .cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03._Star_Enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> attackedPlanets = new Dictionary<string, int>();
+            Dictionary<string, int> destroyedPlanets = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string message = Console.ReadLine();
+                int key = EncriptKey(n, message);
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < message.Length; j++)
+                {
+                    char ch = (char)(message[j] - key);
+                    sb.Append(ch);
+                }
+                string encriptedMessage = sb.ToString();
+                string pattern = @"([^@,\-!:>]*)@(?<planet>[A-Za-z]+)([^@,\-!:>]*):(?<population>\d+)([^@,\-!:>]*)!(?<action>[AD])!([^@,\-!:>]*)->(?<soldierCount>\d+)([^@,\-!:>]*)";
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = regex.Matches(encriptedMessage);
+                foreach (Match match in matches)
+                {
+                    string planet = match.Groups["planet"].Value;
+                    string action = match.Groups["action"].Value;
+                    if (action == "A")
+                    {
+                        if (!attackedPlanets.ContainsKey(planet))
+                        {
+                            attackedPlanets.Add(planet,0);
+                        }
+                        attackedPlanets[planet]++; 
+                    }
+                    else if (action == "D")
+                    {
+                        if (!destroyedPlanets.ContainsKey(planet))
+                        {
+                            destroyedPlanets.Add(planet,0);
+                        }
+                        destroyedPlanets[planet]++;
+                    }
+                }
+
+            }
+            Console.WriteLine($"Attacked planets: {attackedPlanets.Values.Count}");
+            foreach (var attack in attackedPlanets.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"-> {attack.Key}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Values.Count}");
+            foreach (var destroy in destroyedPlanets.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"-> {destroy.Key}");
+            }
+        }
+        private static int EncriptKey(int n,string message)
+        {
+            int count = 0;
+
+            string pattern = @"[S,s,T,t,A,a,R,r]";
+            Regex regex = new Regex(pattern);
+            count = 0;
+            for (int j = 0; j < message.Length; j++)
+            {
+                if (regex.IsMatch(message[j].ToString()))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Technology Fundamentals/Programming Fundamentals Exam - 04 March 2018 Part II/04. ForceBook/04. ForceBook .cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> forceUser = new Dictionary<string, string>();
+            Dictionary<string, int> forceSide = new Dictionary<string, int>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (input.Contains(" | "))
+                {
+                    string[] tokens = input.Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries);
+                    string side = tokens[0];
+                    string name = tokens[1];
+                    if (!forceUser.ContainsKey(name))
+                    {
+                        forceUser.Add(name,side);
+                        if (!forceSide.ContainsKey(side))
+                        {
+                            forceSide.Add(side,0);
+                        }
+                        forceSide[side]++;
+                    }
+                }
+                else if (input.Contains(" -> "))
+                {
+                    string[] tokens = input.Split(new[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                    string name = tokens[0];
+                    string side = tokens[1];
+
+                    if (!forceUser.ContainsKey(name))
+                    {
+                        forceUser.Add(name,side);
+                        if (!forceSide.ContainsKey(side))
+                        {
+                            forceSide.Add(side,0);
+                        }
+                        forceSide[side]++;
+                    }
+                    else
+                    {
+                        forceSide[forceUser[name]]--;
+                        if (!forceSide.ContainsKey(side))
+                        {
+                            forceSide.Add(side,0);
+                        }
+                        forceSide[side]++;
+                        forceUser[name] = side;
+                    }
+                    Console.WriteLine($"{name} joins the {side} side!");
+                }
+            }
+            foreach (var forseSide in forceSide.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                if (forseSide.Value > 0)
+                {
+                    Console.WriteLine($"Side: {forseSide.Key}, Members: {forseSide.Value}");
+                    foreach (var forseUser in forceUser.Where(x => x.Value == forseSide.Key).OrderBy(x => x.Key))
+                    {
+                        Console.WriteLine($"! {forseUser.Key}");
+                    }
+                }
+            }
+        }
+    }
+}
