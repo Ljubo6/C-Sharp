e83e52c43249b939fc9dc989d09452208ccd0e91
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-09 12:27:36 +0300

    Add Exam

---
+++ Technology Fundamentals/Programming Fundamentals Retake Exam - 25 April 2018 Part II/03. Tseam Account/03. Tseam Account.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Tseam_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> account = Console.ReadLine().Split(' ').ToList();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Play!")
+            {
+                string[] tokens = command.Split();
+                string order = tokens[0];
+                string game = tokens[1];
+                if (order == "Install")
+                {
+                    if (game.Contains('-'))
+                    {
+                        continue;
+                    }
+                    if (!account.Contains(game))
+                    {
+                        account.Add(game);
+                    }
+
+                }
+                else if (order == "Uninstall")
+                {
+                    if (game.Contains('-'))
+                    {
+                        continue;
+                    }
+                    if (account.Contains(game))
+                    {
+                        account.Remove(game);
+                    }
+                }
+                else if (order == "Update")
+                {
+                    if (game.Contains('-'))
+                    {
+                        continue;
+                    }
+                    if (account.Contains(game))
+                    {
+                        account.Remove(game);
+                        account.Add(game);
+                    }
+                }
+                else if (order == "Expansion")
+                {
+                    string[] tempArr = game.Split('-');
+                    if (tempArr[0].Contains('-') || tempArr[1].Contains('-'))
+                    {
+                        continue;
+                    }
+                    if (account.Contains(tempArr[0]))
+                    {
+                        game = game.Replace('-', ':').ToString();
+                        int index = account.IndexOf(tempArr[0]);
+                        account.Insert(index + 1, game);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", account));
+        }
+    }
+}
---
+++ Technology Fundamentals/Programming Fundamentals Retake Exam - 25 April 2018 Part II/04. MOBA Challenger/04. MOBA Challenger.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._MOBA_Challenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = string.Empty;
+            Dictionary<string, Dictionary<string, int>> playerSkill = new Dictionary<string, Dictionary<string, int>>();
+            while ((input = Console.ReadLine()) != "Season end")
+            {
+                if (input.Contains("->"))
+                {
+                    string[] tokens = input.Split(new[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                    string player = tokens[0];
+                    string position = tokens[1];
+                    int skill = int.Parse(tokens[2]);
+
+                    if (!playerSkill.ContainsKey(player))
+                    {
+                        playerSkill.Add(player,new Dictionary<string, int>());
+
+                    }
+                    if (!playerSkill[player].ContainsKey(position))
+                    {
+                        playerSkill[player].Add(position, 0);
+                    }
+                    
+                    if (playerSkill[player][position] <= skill)
+                    {
+                        playerSkill[player][position] = skill;
+                    }
+                    
+                }
+                else if (input.Contains("vs"))
+                {
+                    string[] tokens = input.Split(new[] { " vs " }, StringSplitOptions.RemoveEmptyEntries);
+                    string playerOne = tokens[0];
+                    string playerTwo = tokens[1];
+                    if (playerSkill.ContainsKey(playerOne) && playerSkill.ContainsKey(playerTwo))
+                    {
+                        var dictOne = playerSkill[playerOne];
+                        var dictTwo = playerSkill[playerTwo];
+                        foreach (var kvp in dictOne)
+                        {
+                            foreach (var KVP in dictTwo)
+                            {
+                                if (KVP.Key == kvp.Key)
+                                {
+                                    if (kvp.Value > KVP.Value)
+                                    {
+                                        playerSkill.Remove(playerTwo);
+                                    }
+                                    else if (kvp.Value < KVP.Value)
+                                    {
+                                        playerSkill.Remove(playerOne);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }                
+            }
+            foreach (var kvp in playerSkill.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Values.Sum()} skill");
+                var currentDict = kvp.Value;
+                foreach (var KVP in currentDict.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"- {KVP.Key} <::> {KVP.Value}");
+                }
+            }
+        }
+    }
+}
