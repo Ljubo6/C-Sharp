c80e61b2f563c9829cae66e9f41c89d2f2b62253
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-10 13:21:35 +0300

    Add Exams

---
+++ Technology Fundamentals/Programming Fundamentals Exam - 05 November 2017 Part II/03. Anonymous Vox/03. Anonymous Vox .cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03._Anonymous_Vox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            char[] separator = { '{', '}' };
+            string pattern = @"(?<start>[A-Za-z]+)(?<placeholder>.+)\1";
+            MatchCollection matches = Regex.Matches(input,pattern);
+            List<string> strList = new List<string>();
+            string[] values = Console.ReadLine().Split(new[] {'{','}'},StringSplitOptions.RemoveEmptyEntries);
+            foreach (Match match in matches)
+            {
+                string placeholder = match.Groups["placeholder"].Value;
+                strList.Add(placeholder);
+
+            }
+            int end = Math.Min(values.Length,strList.Count);
+
+            for (int i = 0; i < end; i++)
+            {
+                input = ReplaceFirstOccurance(input,strList[i],values[i]);
+            }
+            Console.WriteLine(input);
+        }
+
+        private static string ReplaceFirstOccurance(string input, string match, string replace)
+        {
+            int index = input.IndexOf(match);
+            input = input.Remove(index,match.Length);
+            input = input.Insert(index,replace);
+            return input;
+        }
+    }
+}
---
+++ Technology Fundamentals/Programming Fundamentals Exam - 05 November 2017 Part II/04. Anonymous Cache/04. Anonymous Cache .cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Anonymous_Cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            char[] forbidenSymbols = {' ', '-', '>', '|' };
+            Dictionary<string, Dictionary<string, long>> dict = new Dictionary<string, Dictionary<string, long>>();
+            Dictionary<string, Dictionary<string, long>> cashe = new Dictionary<string, Dictionary<string, long>>();
+            while ((input = Console.ReadLine()) != "thetinggoesskrra")
+            {
+                bool isValid = true;
+                if (input.Contains(" -> "))
+                {
+                    string[] tokens = input.Split(new[] {" -> "," | "},StringSplitOptions.RemoveEmptyEntries);
+                    string dataKey = tokens[0];
+                    int dataSize = int.Parse(tokens[1]);
+                    string dataSet = tokens[2];
+                    for (int i = 0; i < forbidenSymbols.Length; i++)
+                    {
+                        if (dataKey.Contains(forbidenSymbols[i]) && dataSet.Contains(forbidenSymbols[i]))
+                        {
+                            isValid = false;
+                        }
+                    }
+                    if (isValid)
+                    {
+                        if (dict.ContainsKey(dataSet))
+                        {
+                            dict[dataSet].Add(dataKey,dataSize);
+                        }
+                        else
+                        {
+                            if (!cashe.ContainsKey(dataSet))
+                            {
+                                cashe.Add(dataSet, new Dictionary<string, long>());
+                            }
+                            cashe[dataSet].Add(dataKey,dataSize);
+                        }
+                    }
+ 
+                }
+                else
+                {
+                    string dataSet = input;
+                    for (int i = 0; i < forbidenSymbols.Length; i++)
+                    {
+                        if ( dataSet.Contains(forbidenSymbols[i]))
+                        {
+                            isValid = false;
+                        }
+
+                    }
+                    if (isValid)
+                    {
+                        if (cashe.ContainsKey(dataSet))
+                        {
+                            dict.Add(dataSet, new Dictionary<string, long>(cashe[dataSet]));
+                            cashe.Remove(dataSet);
+                        }
+                        else
+                        {
+                            dict.Add(dataSet, new Dictionary<string, long>());
+                        }
+
+                    }
+                }
+            }
+            if (dict.Count > 0)
+            {
+                var newDict = dict.OrderByDescending(x => x.Value.Sum(y => y.Value)).First();
+                Console.WriteLine($"Data Set: {newDict.Key}, Total Size: {newDict.Value.Sum(x => x.Value)}");
+                foreach (var kvp in newDict.Value)
+                {
+                    Console.WriteLine($"$.{kvp.Key}");
+                }
+            }
+        }
+    }
+}
