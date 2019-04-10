b54077004dd0da8d57d08cbd53dd6f708cdf6be8
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-10 23:19:12 +0300

    Add Exam

---
+++ Technology Fundamentals/Programming Fundamentals Exam - 09 July 2017 Part 2/03. Regexmon/03. Regexmon .cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Regexmon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bojomonPattern = @"[A-Za-z]+-[A-Za-z]+";
+            string didimonPattern = @"[^-A-Za-z]+";
+            Regex bojoRegex = new Regex(bojomonPattern);
+            Regex didiRegex = new Regex(didimonPattern);
+            string input = Console.ReadLine();
+            while (input != string.Empty)
+            {
+                if (didiRegex.IsMatch(input))
+                {
+                    string didiMatch = didiRegex.Match(input).Value;
+                    Console.WriteLine(didiMatch);
+
+                    int didiStartIndex = input.IndexOf(didiMatch);
+                    input = input.Remove(0,didiStartIndex + didiMatch.Length);
+                }
+                else
+                {
+                    break;
+                }
+
+                if (bojoRegex.IsMatch(input))
+                {
+                    string bojoMatch = bojoRegex.Match(input).Value;
+                    Console.WriteLine(bojoMatch);
+
+                    int bojoStartIndex = input.IndexOf(bojoMatch);
+                    input = input.Remove(0,bojoStartIndex + bojoMatch.Length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    } 
+}
---
+++ Technology Fundamentals/Programming Fundamentals Exam - 09 July 2017 Part 2/04. Pokemon Evolution/04. Pokemon Evolution .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Pokemon_Evolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            while ((input = Console.ReadLine()) != "Wubbalubbadubdub")
+            {
+                string[] tokens = input.Split(new[] {" -> "},StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                if (tokens.Length > 1)
+                {
+                    string typeIndex = $"{tokens[1]} <-> {tokens[2]}";
+                    if (!dict.ContainsKey(name))
+                    {
+                        dict.Add(name,new List<string>());
+                    }
+                    dict[name].Add(typeIndex);
+                }
+                else
+                {
+                    if (dict.ContainsKey(name))
+                    {
+                        Console.WriteLine($"# {name}");
+                        foreach (var item in dict[name])
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+
+                }
+            }
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"# {kvp.Key}");
+                foreach (var item in kvp.Value
+                    .OrderByDescending(x => int.Parse(x.Split(new[] {" <-> "},StringSplitOptions.RemoveEmptyEntries).Skip(1).First())))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
