7807a973d9fd232c97f96c4d2ca9cd17489384e8
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-04-23 22:59:27 +0300

    Add Final Exam

---
+++ Technology Fundamentals/TechnologyFundamentalsFinal Exam - 14 April 2019 Group I/01. Arriving in Kathmandu/01. Arriving in Kathmandu .cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Arriving_in_Kathmandu
+{
+    class Program
+    {
+        private static object regex;
+
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Last note")
+            {
+                StringBuilder sb = new StringBuilder();
+                int n = 0;
+                string patternNumber = @"(?<=\=)([0-9]+)";
+                Regex regexNumber = new Regex(patternNumber);
+                Match num = regexNumber.Match(input);
+                if (num.Success)
+                {
+                    n = int.Parse(num.Value.ToString());
+                }
+                string pattern = $@"^(?<name>[A-Za-z0-9!@#$?]+)=(?<number>([0-9]+))<<(?<coordinate>.{{{n}}}$)";
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].Value.ToString();
+                    for (int i = 0; i < name.Length; i++)
+                    {
+                        if (char.IsLetterOrDigit(name[i]))
+                        {
+                            sb.Append(name[i]);
+                        }
+                    }
+                    name = sb.ToString();
+                    string coorditane = match.Groups["coordinate"].Value.ToString();
+
+                    Console.WriteLine($"Coordinates found! {name} -> {coorditane}");
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+                
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals/TechnologyFundamentalsFinal Exam - 14 April 2019 Group I/02. On the Way to Annapurna/02. On the Way to Annapurna.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._On_the_Way_to_Annapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> storeAndItems = new Dictionary<string, List<string>>();
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(new[] { "->","," },StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] == "Add" && tokens.Length == 3)
+                {
+                    string store = tokens[1];
+                    string item = tokens[2];
+                    if (!storeAndItems.ContainsKey(store))
+                    {
+                        storeAndItems.Add(store,new List<string>());
+                    }
+                    storeAndItems[store].Add(item);
+                }
+                else if (tokens[0] == "Add" && tokens.Length > 3)
+                {
+                    string store = tokens[1];
+                    if (!storeAndItems.ContainsKey(store))
+                    {
+                        storeAndItems.Add(store,new List<string>());
+                    }
+                    for (int i = 2; i < tokens.Length; i++)
+                    {
+                        storeAndItems[store].Add(tokens[i]);
+                    }
+                    
+                }
+                else if (tokens[0] == "Remove" && tokens.Length == 2)
+                {
+                    string store = tokens[1];
+                    storeAndItems.Remove(store);
+                }
+            }
+            Console.WriteLine("Stores list:");
+            foreach (var kvp in storeAndItems
+                .OrderByDescending(x => x.Value.Count())
+                .ThenByDescending(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}");
+                var list = kvp.Value;
+                foreach (var item in kvp.Value)
+                {
+                    Console.WriteLine($"<<{item}>>");
+                }
+            }
+        }
+    }
+}
