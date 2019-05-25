82a8cfb63ef09349c4572f321627f2e7bd552226
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-05-25 20:57:50 +0300

    Add Lab

---
+++ C# Advanced/Generics - Lab/01.Box/Box{T}.cs
@@ -0,0 +1,33 @@
+﻿namespace BoxOfT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public class Box<T>
+    {
+        private List<T> items;
+        public Box()
+        {
+            this.items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            this.items.Add(item);
+        }
+        public T Remove()
+        {
+            var rem = this.items.Last();
+            this.items.RemoveAt(this.items.Count - 1);
+            return rem;
+        }
+        public int Count
+        {
+            get
+            {
+                return this.items.Count;
+            }
+            
+        }
+    }
+}
---
+++ C# Advanced/Generics - Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericScale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class EqualityScale<T>
+        where T : IComparable<T>
+    {
+        private T first;
+        private T second;
+
+        public EqualityScale(T first, T second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool AreEqual()
+        {
+            bool result = this.first.Equals(this.second);
+            return result;
+        }
+    }
+}
