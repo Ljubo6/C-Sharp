ed94552514a587b01bd5ff7090d8112414ac82e7
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-06-06 23:25:29 +0300

    Repair Classes

--- C# Advanced/WorkShop/CustomList/CustList.cs
+++ C# Advanced/WorkShop/CustomList/CustList.cs
@@ -1,11 +1,21 @@
 ﻿namespace CustomList
 {
     using System;
+    using System.Collections.Generic;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            CustomListList temp = new CustomListList();
+            temp.Add(1);
+            temp.Add(2);
+            temp.Add(3);
+            temp.Add(4);
+            temp.Add(5);
+            temp.Add(6);
+            temp.Insert(0,7);
+            Console.WriteLine();
         }
     }
 }
