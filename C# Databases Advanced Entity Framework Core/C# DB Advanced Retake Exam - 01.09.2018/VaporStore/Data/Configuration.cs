701dd11046a4c4591f2de14635c0b460ede365a2
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-12-06 13:22:05 +0200

    deleted

--- C# Databases Advanced Entity Framework Core/C# DB Advanced Retake Exam - 01.09.2018/VaporStore/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VaporStore.Data
-{
-	public static class Configuration
-	{
-		public static string ConnectionString =
-            @"Server=DESKTOP-OE6JNPK\SQLEXPRESS;Database=VaporStore;Trusted_Connection=True";
-	}
-}
