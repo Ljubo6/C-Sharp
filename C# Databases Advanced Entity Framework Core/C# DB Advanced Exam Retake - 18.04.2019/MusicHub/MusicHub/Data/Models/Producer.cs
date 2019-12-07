20247a6603b7af70317cf25bf1d79f084276333a
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2019-12-07 22:42:08 +0200

    Add Exam

---
+++ C# Databases Advanced Entity Framework Core/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public Producer()
+        {
+            this.Albums = new HashSet<Album >();
+        }
+        public int Id { get; set; }
+
+        [StringLength(30,MinimumLength = 3),Required]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+$")]
+        public string Pseudonym { get; set; }
+
+        [RegularExpression(@"^\+359 [0-9]+ [0-9]+ [0-9]+$")]
+        public string PhoneNumber { get; set; }
+        public ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ C# Databases Advanced Entity Framework Core/C# DB Advanced Exam Retake - 18.04.2019/MusicHub/MusicHub/DataProcessor/ImportDtos/ImportSongXmlDto.cs
@@ -0,0 +1,33 @@
+﻿using MusicHub.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MusicHub.DataProcessor.ImportDtos
+{
+    [XmlType("Song")]
+    public class ImportSongXmlDto
+    {
+        [StringLength(20, MinimumLength = 3), Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Duration { get; set; }
+
+        [Required]
+        public string CreatedOn { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        public int? AlbumId { get; set; }
+
+        [Required]
+        public int WriterId { get; set; }
+
+        [Range(typeof(decimal), "0.0", "79228162514264337593543950335"), Required]
+        public decimal Price { get; set; }
+    }
+}
